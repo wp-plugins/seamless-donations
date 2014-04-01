bd5212f2bb0682ad986a449e7d7d58337b11284b
allendav <allendav@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-01 05:05:19 +0000

    tagging version 3.2.2
    git-svn-id: https://plugins.svn.wordpress.org/seamless-donations/tags/3.2.2@885473 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-views/export.php
+++ admin-views/export.php
@@ -1,21 +1,8 @@
 <?php
-/*  Copyright 2013 Allen Snook (email: allen@allensnook.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
+/* Copyright 2013 Allen Snook (email: allendav@allendav.com) */
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */ 
-
-include( '../../../wp-config.php' );
+include( '../../../../wp-config.php' );
 
 if ( ! current_user_can( 'manage_options' ) ) {
 
@@ -37,6 +24,7 @@ else
 
 	$startTimeStamp = strtotime( $startDate );
 	$endTimeStamp = strtotime( $endDate );
+	$countries = dgx_donate_get_countries();
 
 	$firstOne = true;
 
@@ -83,9 +71,10 @@ else
 				header( "Content-type: text/csv" );
 				header( "Content-Disposition: attachment; filename=export.csv" );
 
-				echo "\"Date\",\"Time\",\"First Name\",\"Last Name\",\"Amount\",\"Repeating\",";
+				echo "\"Date\",\"Time\",\"First Name\",\"Last Name\",\"Amount\",\"Currency\",\"Repeating\",";
 				echo "\"Designated Fund\",\"Gift Item\",\"Phone\",\"Email\",\"Address\",\"Address 2\",";
-				echo "\"City\",\"State\",\"Zip\",\"OK to Add to Mailing List\"\n";
+				echo "\"City\",\"State/Prov\",\"Postal Code\",\"Country\",\"Employer\",\"Occupation\",";
+				echo "\"OK to Add to Mailing List\"\n";
 	
 				$firstOne = false;
 			}
@@ -94,7 +83,9 @@ else
 			$firstName = get_post_meta( $donationID, '_dgx_donate_donor_first_name', true );
 			$lastName = get_post_meta( $donationID, '_dgx_donate_donor_last_name', true );
 			$amount = get_post_meta( $donationID, '_dgx_donate_amount', true );
-			$formattedAmount = "$" . number_format( $amount, 2 );
+
+			$currency_code = dgx_donate_get_donation_currency_code( $donationID );
+			$formatted_amount = dgx_donate_get_plain_formatted_amount( $amount, 2, $currency_code, false );
 			$repeating = get_post_meta( $donationID, '_dgx_donate_repeating', true );
 			if ( empty( $repeating ) )
 			{
@@ -121,6 +112,24 @@ else
 			$address2 = get_post_meta( $donationID, '_dgx_donate_donor_address2', true );
 			$city = get_post_meta( $donationID, '_dgx_donate_donor_city', true );
 			$state = get_post_meta( $donationID, '_dgx_donate_donor_state', true );
+			$province = get_post_meta( $donationID, '_dgx_donate_donor_province', true );
+			$country_code = get_post_meta( $donationID, '_dgx_donate_donor_country', true );
+
+			if ( empty( $country_code ) ) { /* older versions only did US */
+				$country_code = 'US';
+				update_post_meta( $donationID, '_dgx_donate_donor_country', 'US' );
+			}
+
+			if ( 'US' == $country_code ) {
+				$state_or_prov = $state;
+			} else if ( 'CA' == $country_code ) {
+				$state_or_prov = $province;
+			} else {
+				$state_or_prov = '';
+			}
+
+			$country = $countries[$country_code];
+
 			$zip = get_post_meta( $donationID, '_dgx_donate_donor_zip', true );
 			$addToMailingList = get_post_meta( $donationID, '_dgx_donate_add_to_mailing_list', true );
 			if ( strcasecmp( $addToMailingList, 'on' ) == 0 )
@@ -130,11 +139,15 @@ else
 			else
 			{
 				$addToMailingList = "No";
-			}	
+			}
 
-			echo "\"$donationDate\",\"$time\",\"$firstName\",\"$lastName\",\"$formattedAmount\",\"$repeating\",";
+			$employer = get_post_meta( $donationID, '_dgx_donate_employer_name', true );
+			$occupation = get_post_meta( $donationID, '_dgx_donate_occupation', true );
+
+			echo "\"$donationDate\",\"$time\",\"$firstName\",\"$lastName\",\"$formatted_amount\",\"$currency_code\",\"$repeating\",";
 			echo "\"$designatedFundName\",\"$gift_item_title\",\"$phone\",\"$email\",\"$address\",\"$address2\",";
-			echo "\"$city\",\"$state\",\"$zip\",\"$addToMailingList\"\n";
+			echo "\"$city\",\"$state_or_prov\",\"$zip\",\"$country\",\"$employer\",\"$occupation\",";
+			echo "\"$addToMailingList\"\n";
 		}
 	}
 
